e391186857b1fad4c045c220ac2cebb3c5416fa6
I001962 <i001962@gmail.com>
2026-08-05 00:03:37 -0700

    Clarify cast submit return path

--- components/sendCastPrivy.ts
+++ components/sendCastPrivy.ts
@@ -2,27 +2,29 @@ import { ExternalEd25519Signer, HubRestAPIClient } from '@standard-crypto/farcas
 import { FarcasterHub } from '../constants/constants';
 
 const sendCastPrivy = async (
-    casterFID: number,
-    newPost: string, 
-    targetUrl: string,
-    privySigner: ExternalEd25519Signer,
-    ) => {
-      console.log('privySigner FID', casterFID, privySigner);
+  casterFID: number,
+  newPost: string,
+  targetUrl: string,
+  privySigner: ExternalEd25519Signer,
+) => {
+  console.log('privySigner FID', casterFID, privySigner);
 
-      const client = new HubRestAPIClient({
-        //hubUrl: 'https://hubs-grpc.airstack.xyz:2281', needs header apikey ??
-        hubUrl: FarcasterHub, 
-      });
-      console.log('privySigner', privySigner);
-      if (privySigner === undefined) {
-        console.error('privySigner is undefined');
-        return;
-      }
-      const submitCastResponse = await client.submitCast(
-        {text: newPost, parentUrl: targetUrl},
-        casterFID,
-        privySigner,
-      );
+  const client = new HubRestAPIClient({
+    hubUrl: FarcasterHub,
+  });
+
+  if (privySigner === undefined) {
+    console.error('privySigner is undefined');
+    return;
+  }
+
+  const submitCastResponse = await client.submitCast(
+    { text: newPost, parentUrl: targetUrl },
+    casterFID,
+    privySigner,
+  );
+
+  return submitCastResponse;
 };
 
 export default sendCastPrivy;
